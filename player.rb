710b8b56461fa8ee84254f24d5e51dea74454b36
yoshikou820 <yoshikou0507@icloud.com>
2019-08-20 13:06:18 +0900

    create player.rb

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player < Sprite
+	def initialize(x,y,image)
+
+	end
+	def up
+		self.y -= 5
+	end
+	def down
+		self.y +=5
+	end
+	def right
+		self.x += 5
+	end
+	def left
+		self.x -= 5
+	end
+end
